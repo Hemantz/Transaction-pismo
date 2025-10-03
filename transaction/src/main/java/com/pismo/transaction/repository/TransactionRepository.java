f371973e3c3dd04b840096f495814d3ce88803e9
Hemant Bhardwaj <34510460+Hemantz@users.noreply.github.com>
2025-10-03 23:58:31 +0530

    Update TransactionRepository.java

--- transaction/src/main/java/com/pismo/transaction/repository/TransactionRepository.java
+++ transaction/src/main/java/com/pismo/transaction/repository/TransactionRepository.java
@@ -4,7 +4,13 @@ package com.pismo.transaction.repository;
 
 import com.pismo.transaction.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    @Query(value = "SELECT * FROM TRANSACTIONS WHERE ACCOUNT_ID = :accountId AND BALANCE < 0 ORDER BY Transaction_Id ASC", nativeQuery = true)
+    List<Transaction> findAllDebitTransaction(Long accountId);
 }
